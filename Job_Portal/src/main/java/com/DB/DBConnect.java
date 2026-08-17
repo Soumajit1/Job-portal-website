39faa975121efd1744ae4eb92fd06641aac4fbc1
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-08-17 17:31:08 +0530

    Update DBConnect.java

--- Job_Portal/src/main/java/com/DB/DBConnect.java
+++ Job_Portal/src/main/java/com/DB/DBConnect.java
@@ -11,9 +11,28 @@ public class DBConnect {
 
         try {
 
-            if (conn == null) {
+            if (conn == null || conn.isClosed()) {
+
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal", "root", "@Soumajit2006");
+
+                String host = System.getenv("DB_HOST");
+                String port = System.getenv("DB_PORT");
+                String database = System.getenv("DB_NAME");
+                String username = System.getenv("DB_USER");
+                String password = System.getenv("DB_PASSWORD");
+
+                String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                        + "?useSSL=true"
+                        + "&requireSSL=true"
+                        + "&serverTimezone=UTC"
+                        + "&useUnicode=true"
+                        + "&characterEncoding=UTF-8";
+
+                conn = DriverManager.getConnection(
+                        url,
+                        username,
+                        password
+                );
             }
 
         } catch (Exception e) {
